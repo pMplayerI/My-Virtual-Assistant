52a43c3f5bf28208091e51b678d8f19026dfe76a
pMplayerI <cusstop386@gmail.com>
2023-05-01 02:47:09 +0700

    Add weather() and improve code

--- MyVirtualAssistant.py
+++ MyVirtualAssistant.py
@@ -5,10 +5,13 @@ import playsound
 from gtts import gTTS
 from datetime import date, datetime
 import smtplib
+import pyjokes
+from deep_translator import GoogleTranslator
+import requests
+import json
 
 # Tên bot
 botName = "Bảo"
-now = datetime.now()
 
 # Nghe, hiểu và nói
 def listen():
@@ -20,109 +23,95 @@ def listen():
         audio = ear.listen(mic)
     print("Robot: ...")
     try:
-        text = ear.recognize_google(audio, language="vi-VN")      
+        you = ear.recognize_google(audio, language="vi-VN")      
     except:
-        text = "..."
+        you = "..."
 
-    print("You: " +text)
-    return text
+    print("You: " +you)
+    return you
     
 def understand():  
-    text = listen()
-    return text
+    brain = listen()
+    return brain
 
 def speak(brain):
     print("Robot: " +brain+ "\n")
-    output = gTTS(brain, lang="vi", slow=False)
-    output.save("voice.mp3")
+    mouth = gTTS(brain, lang="vi", slow=False)
+    mouth.save("voice.mp3")
     playsound.playsound("voice.mp3", True)  
     # Không xóa là lỗi
     os.remove("voice.mp3")
 
 # Các chức năng chính
 def hello(name):
-    dayTime = int(now.strftime("%H"))
+    dayTime = int(datetime.now().strftime("%H"))
     if 6 <= dayTime < 12:
-        brain = "Chào bạn " +name+ "! Chúc bạn một buổi sáng tốt lành!"
-        speak(brain)
+        speak("Chào bạn " +name+ "! Chúc bạn một buổi sáng tốt lành!")
     elif 12 <= dayTime < 18:
-        brain = "Chào bạn " +name+ "! Chúc bạn một buổi chiều tốt lành!"
-        speak(brain)
+        speak("Chào bạn " +name+ "! Chúc bạn một buổi chiều tốt lành!")
     else:
-        brain = "Chào bạn " +name+ "! Chúc bạn một buổi tối tốt lành!"
-        speak(brain)
+        speak("Chào bạn " +name+ "! Chúc bạn một buổi tối tốt lành!")
 
-def getToday():
-    today = date.today()
-    brain = today.strftime("Hôm nay là ngày %d tháng %m năm %Y.")
-    speak(brain)
+def getToday(): 
+    speak(date.today().strftime("Hôm nay là ngày %d tháng %m năm %Y."))
 
 def getTime():
-    brain = now.strftime("%H:%M:%S")
-    speak(brain)
+    speak(datetime.now().strftime("%H:%M:%S"))
 
 def searching(text):
     search = text.split("kiếm", 1)[1]
-    brain = "Đang tìm kiếm!"
-    speak(brain)
+    speak("Đang tìm kiếm!")
     url = "https://www.google.com.tr/search?q=" +search
     webbrowser.open(url, new=1)
-    brain = "Đã tìm kiếm thành công!"
-    speak(brain)
+    speak("Đã tìm kiếm thành công!")
 
 def openWebsite(text):
     # Lấy dấu cách là không tìm được
     search = text.split("mở ", 1)[1]
-    brain = "Đang mở trang web!"
-    speak(brain)
+    speak("Đang mở trang web!")
     url = "https://www." +search
     webbrowser.open(url, new=1)
-    brain = "Trang web bạn yêu cầu đã được mở!"
-    speak(brain)
+    speak("Trang web bạn yêu cầu đã được mở!")
 
 def openApp(text):
     if "google" in text or "chrome" in text:
-        brain = "Đang mở Google Chrome!"
-        speak(brain)
+        speak("Đang mở Google Chrome!")
         os.startfile("C:\Program Files\Google\Chrome\Application\chrome.exe")
-        brain = "Google Chrome đã được mở!"
-        speak(brain)
+        speak("Google Chrome đã được mở!")
 
     elif "code" in text:
-        brain = "Đang mở Visual Studio Code!"
-        speak(brain)
+        speak("Đang mở Visual Studio Code!")
         # Có cái r đó máy t mới mở được
         os.startfile(r"C:\Users\Admin\AppData\Local\Programs\Microsoft VS Code\Code.exe")
-        brain = "Visual Studio Code đã được mở!"
-        speak(brain)
+        speak("Visual Studio Code đã được mở!")
 
     elif "game" in text:
-        brain = "Đang mở LMHT!"
-        speak(brain)
+        speak("Đang mở LMHT!")
         os.startfile("D:\Riot Games\Riot Client\RiotClientServices.exe")
-        brain = "Chơi game ít thôi!"
-        speak(brain)
+        speak("Chơi game ít thôi!")
+
+    elif "notepad" in text:
+        speak("Đang mở Notepad!")
+        os.startfile(r"C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad")
+        speak("Notepad đã được mở!")
 
     else:
-        brain = "404 not found!"
-        speak(brain)
+        speak("Không tìm thấy ứng dụng!")
 
 def sendEmail():
     sender = "primegaming737@gmail.com"
     # Đây là mật khẩu ứng dụng
     password = "cklylijydznawvwf"
 
-    brain = "Bạn muốn gửi email đến ai?"
-    speak(brain)
+    speak("Bạn muốn gửi email đến ai?")
     receiver = understand()
 
+    # Thử nghiệm
     if "tôi" in receiver:
-        brain = "Nội dung của email là gì?"
-        speak(brain)
+        speak("Nội dung của email là gì?")
         content = understand()
         message = "Subject: %s\n\n%s"%("Email tự động", content)
-        brain = "Email đang được gửi!"
-        speak(brain)
+        speak("Email đang được gửi!")
 
         session = smtplib.SMTP("smtp.gmail.com", 587)
         # Xác minh email với smtp gmail client
@@ -133,49 +122,72 @@ def sendEmail():
         session.sendmail(sender, "cusstop386@gmail.com", message.encode("utf-8"))
         session.close()
 
-        brain = "Đã gửi email thành công!"
-        speak(brain)
+        speak("Đã gửi email thành công!")
 
     else:
-        brain = "Không thành công!"
-        speak(brain)
+        speak("Gửi email thành công!")
 
+def weather():
+    speak("Bạn muốn xem thời tiết ở địa điểm nào?")
 
+    city = understand()
+    if city == "...":
+        city = "Thành phố Hồ Chí Minh"
+
+    url = "http://api.openweathermap.org/data/2.5/weather?appid=fe8d8c65cf345889139d8e545f57819a&q=" +city+ "&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] != "404":
+        city_res = data["main"]
+        current_temperature = city_res["temp"]
+        current_pressure = city_res["pressure"]
+        current_humidity = city_res["humidity"]
+
+        suntime = data["sys"]
+        sunrise = datetime.fromtimestamp(suntime["sunrise"])
+        sunset = datetime.fromtimestamp(suntime["sunset"])
+
+        wthr = data["weather"]
+        weather_description = wthr[0]["description"]
+        
+        speak("\nHôm nay ở " +city+ " sẽ có " +GoogleTranslator(source="auto", target="vi").translate(weather_description)+ "\nMặt trời mọc vào lúc " +str(sunrise.hour)+ " giờ " +str(sunrise.minute)+ " phút\nMặt trời lặn vào lúc " +str(sunset.hour)+ " giờ " +str(sunset.minute)+ " phút\nNhiệt độ trung bình là " +str(current_temperature)+ " độ C\nÁp suất không khí là " +str(current_pressure)+ " Pa\nĐộ ẩm là " +str(current_humidity)+ "%")
+
+    else:
+        speak("Không tìm thấy địa điểm của bạn!")
 
 # Tán gẫu
 def opening(name):
-    brain = "Chào bạn " +name+ "! Tôi có thể giúp gì cho bạn?"
-    speak(brain)
+    speak("Chào bạn " +name+ "! Tôi có thể giúp gì cho bạn?")
 
 def stop():
-    brain = "Hẹn gặp lại!"
-    speak(brain)
+    speak("Hẹn gặp lại!")
 
 def noisy():
-    brain = "Tôi không nghe rõ! Bạn có thể nhắc lại được không?"
-    speak(brain)
+    speak("Tôi không nghe rõ! Bạn có thể nhắc lại được không?")
 
 def baoNgu():
-    brain = "Tôi là " +botName+ ", một nô lệ của bạn!"
-    speak(brain)
+    speak("Tôi là " +botName+ ", một nô lệ của bạn!")
 
 def stillListening():
-    brain = "Vâng, tôi vẫn đang lắng nghe!"
-    speak(brain)
+    speak("Vâng, tôi vẫn đang lắng nghe!")
 
 def compliment():
-    brain = "Ok baby!"
-    speak(brain)
+    speak("Ok baby!")
+
+def feeling():
+    speak("Tôi khỏe, cảm ơn, còn bạn!")
+
+def joke():
+    speak(GoogleTranslator(source="auto", target="vi").translate(pyjokes.get_joke(language="en", category="all")))
 
 def nope():
-    brain = "Tôi khỏe cảm ơn còn bạn!"
-    speak(brain)
+    speak("Tôi không biết!")
 
 # AI
 def ai():
     # Mở đầu
-    brain = "Xin chào, bạn tên là gì?"
-    speak(brain)
+    speak("Xin chào, bạn tên là gì?")
     name = understand()
     opening(name)
 
@@ -206,6 +218,9 @@ def ai():
         elif "mail" in text:
             sendEmail()
 
+        elif "thời tiết" in text:
+            weather()
+
         # Xử lý tán gẫu
         elif "tạm biệt" in text:
             stop()
@@ -223,6 +238,12 @@ def ai():
 
         elif "tốt" in text or "giỏi" in text or "cảm ơn" in text:
             compliment()
+
+        elif "khỏe không" in text:
+            feeling()
+
+        elif "cười" in text:
+            joke()
 
         # Hỏi gì khó vậy 3
         else:
